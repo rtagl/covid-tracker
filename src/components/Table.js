c410c51e4e8db0221b5b3524ceb12928b359ba3e
Ric Taglietti <rictaglietti@gmail.com>
2021-10-18 11:49:17 -0400

    add worldwide data and chart

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,14 +3,18 @@ import '../Table.css';
 const Table = ({ countries }) => {
   return (
     <div className="table">
-      {countries.map(({ country, cases, i }) => (
-        <tr key={country}>
-          <td>{country}</td>
-          <td>
-            <strong>{cases}</strong>
-          </td>
-        </tr>
-      ))}
+      <table>
+        <tbody>
+          {countries.map(({ country, cases, i }) => (
+            <tr key={country}>
+              <td>{country}</td>
+              <td>
+                <strong>{cases}</strong>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
